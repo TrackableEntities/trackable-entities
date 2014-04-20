775c38988f8bd9fed67575ad278b24a5fad89568
tonysneed <tony@tonysneed>
2014-04-20 06:33:39 +0200

    Updated customer controller using updated t4 template.

--- Samples/VS2013/WebApiSample/WebApiSample.WebApi/Controllers/CustomerController.cs
+++ Samples/VS2013/WebApiSample/WebApiSample.WebApi/Controllers/CustomerController.cs
@@ -6,127 +6,140 @@ using System.Data.Entity.Infrastructure;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using TrackableEntities.Common;
+using TrackableEntities;
 using TrackableEntities.EF6;
+using TrackableEntities.Common;
 using WebApiSample.Service.Entities.Models;
 
 namespace WebApiSample.Service.WebApi.Controllers
 {
-	public class CustomerController : ApiController
-	{
-		private readonly NorthwindSlimContext _dbContext = new NorthwindSlimContext();
+    public class CustomerController : ApiController
+    {
+        private readonly NorthwindSlimContext _dbContext = new NorthwindSlimContext();
 
-		// GET api/Customer
-		[ResponseType(typeof(IEnumerable<Customer>))]
-		public async Task<IHttpActionResult> GetCustomers()
-		{
-			IEnumerable<Customer> customers = await _dbContext.Customers.ToListAsync();
+        // GET api/Customer
+        [ResponseType(typeof(IEnumerable<Customer>))]
+        public async Task<IHttpActionResult> GetCustomers()
+        {
+	        IEnumerable<Customer> customers = await _dbContext.Customers
+				.Include(c => c.CustomerSetting)
+				.ToListAsync();
 	
-			return Ok(customers);
-		}
+            return Ok(customers);
+        }
 
-		// GET api/Customer/5
-		[ResponseType(typeof(Customer))]
-		public async Task<IHttpActionResult> GetCustomer(string id)
-		{
-			Customer customer = await _dbContext.Customers.SingleOrDefaultAsync(c => c.CustomerId == id);
+        // GET api/Customer/5
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> GetCustomer(string id)
+        {
+	        Customer customer = await _dbContext.Customers
+				.Include(c => c.CustomerSetting)
+				.SingleOrDefaultAsync(c => c.CustomerId == id);
 	
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // POST api/Customer
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> PostCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            customer.TrackingState = TrackingState.Added;
+            _dbContext.ApplyChanges(customer);
+
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (_dbContext.Customers.Any(c => c.CustomerId == customer.CustomerId))
+                {
+                    return Conflict();
+                }
+                throw;
+            }
+
+            await _dbContext.LoadRelatedEntitiesAsync(customer);
+            customer.AcceptChanges();
+            return CreatedAtRoute("DefaultApi", new { id = customer.CustomerId }, customer);
+        }
+
+        // PUT api/Customer
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> PutCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dbContext.ApplyChanges(customer);
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_dbContext.Customers.Any(c => c.CustomerId == customer.CustomerId))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+			await _dbContext.LoadRelatedEntitiesAsync(customer);
+			customer.AcceptChanges();
+	        return Ok(customer);
+        }
+
+        // DELETE api/Customer/5
+        public async Task<IHttpActionResult> DeleteCustomer(string id)
+        {
+			Customer customer = await _dbContext.Customers
+				.SingleOrDefaultAsync(c => c.CustomerId == id);
 			if (customer == null)
-			{
-				return NotFound();
-			}
-			return Ok(customer);
-		}
+            {
+                return Ok();
+            }
 
-		// PUT api/Customer
-		[ResponseType(typeof(Customer))]
-		public async Task<IHttpActionResult> PutCustomer(Customer customer)
-		{
-			if (!ModelState.IsValid)
-			{
-				return BadRequest(ModelState);
-			}
+			customer.TrackingState = TrackingState.Deleted;
+			_dbContext.ApplyChanges(customer);
 
-			try
-			{
-				// Update object graph entity state
-				_dbContext.ApplyChanges(customer);
-				await _dbContext.SaveChangesAsync();
-				customer.AcceptChanges();
-				return Ok(customer);
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!_dbContext.Customers.Any(c => c.CustomerId == customer.CustomerId))
-				{
-					return NotFound();
-				}
-				throw;
-			}
-		}
+            try
+            {
+	            await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_dbContext.Customers.Any(c => c.CustomerId == customer.CustomerId))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
 
-		// POST api/Customer
-		[ResponseType(typeof(Customer))]
-		public async Task<IHttpActionResult> PostCustomer(Customer customer)
-		{
-			if (!ModelState.IsValid)
-			{
-				return BadRequest(ModelState);
-			}
+            return Ok();
+        }
 
-			_dbContext.Customers.Add(customer);
-
-			try
-			{
-				await _dbContext.SaveChangesAsync();
-				customer.AcceptChanges();
-			}
-			catch (DbUpdateException)
-			{
-				if (_dbContext.Customers.Any(c => c.CustomerId == customer.CustomerId))
-				{
-					return Conflict();
-				}
-				throw;
-			}
-
-			return CreatedAtRoute("DefaultApi", new { id = customer.CustomerId }, customer);
-		}
-
-		// DELETE api/Customer/5
-		public async Task<IHttpActionResult> DeleteCustomer(string id)
-		{
-			Customer customer = await _dbContext.Customers.SingleOrDefaultAsync(c => c.CustomerId == id);
-			if (customer == null)
-			{
-				return NotFound();
-			}
-
-			_dbContext.Customers.Attach(customer);
-			_dbContext.Customers.Remove(customer);
-
-			try
-			{
-				await _dbContext.SaveChangesAsync();
-				return Ok();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!_dbContext.Customers.Any(c => c.CustomerId == customer.CustomerId))
-				{
-					return NotFound();
-				}
-				throw;
-			}
-		}
-
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				_dbContext.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-	}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
