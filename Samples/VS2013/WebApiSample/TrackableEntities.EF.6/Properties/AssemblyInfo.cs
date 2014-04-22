1386ce3f2c2255cd276f6cf96d82367666f82dfc
tonysneed <tony@tonysneed>
2014-04-22 13:04:53 +0200

    Updated VS2012 templates and VSIX installer.

---
+++ Samples/VS2013/WebApiSample/TrackableEntities.EF.6/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TrackableEntities.EF.6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrackableEntities.EF.6")]
+[assembly: AssemblyCopyright("Copyright © Tony Sneed 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3bf4f4e-009e-4c6b-b519-0bc85547f3f9")]
