934a92eaa3f8c58a6b88c2430a7418ca4be59547
tonysneed <tony@tonysneed>
2014-04-24 06:53:42 +0200

    Added Relations sample.

---
+++ Samples/VS2013/WebApiSample.Relations/WebApiSample.Service.Entities/Contexts/NorthwindSlimDatabaseInitializer.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using WebApiSample.Service.Entities.Models;
+
+namespace WebApiSample.Service.Entities.Contexts
+{
+    public class NorthwindSlimDatabaseInitializer : DropCreateDatabaseIfModelChanges<NorthwindSlimContext>
+    {
+        protected override void Seed(NorthwindSlimContext context)
+        {
+            AddCategories(context);
+            AddProducts(context);
+            AddCustomers(context);
+            AddCustomerSettings(context);
+            AddOrders(context);
+            AddEmployees(context);
+        }
+
+        private void AddOrders(NorthwindSlimContext context)
+        {
+            context.Orders.Add(new Order
+            {
+                CustomerId = "ALFKI",
+                OrderId = 1,
+                OrderDate = DateTime.Today,
+                ShippedDate = DateTime.Today,
+                Freight = 41.34M,
+                OrderDetails = new List<OrderDetail>
+                {
+                    new OrderDetail
+                    {
+                        ProductId = 1,
+                        Quantity = 1,
+                        UnitPrice = 10M
+                    },
+                    new OrderDetail
+                    {
+                        ProductId = 2,
+                        Quantity = 2,
+                        UnitPrice = 20M
+                    },
+                }
+            });
+            context.Orders.Add(new Order
+            {
+                CustomerId = "ALFKI",
+                OrderId = 2,
+                OrderDate = DateTime.Today,
+                ShippedDate = DateTime.Today,
+                Freight = 41.34M,
+                OrderDetails = new List<OrderDetail>
+                {
+                    new OrderDetail
+                    {
+                        ProductId = 3,
+                        Quantity = 3,
+                        UnitPrice = 30M
+                    },
+                }
+            });
+            context.Orders.Add(new Order
+            {
+                CustomerId = "ANATR",
+                OrderId = 3,
+                OrderDate = DateTime.Today,
+                ShippedDate = DateTime.Today,
+                Freight = 41.34M,
+                OrderDetails = new List<OrderDetail>
+                {
+                    new OrderDetail
+                    {
+                        ProductId = 2,
+                        Quantity = 4,
+                        UnitPrice = 20M
+                    },
+                }
+            });
+            context.Orders.Add(new Order
+            {
+                CustomerId = "ANTON",
+                OrderId = 4,
+                OrderDate = DateTime.Today,
+                ShippedDate = DateTime.Today,
+                Freight = 41.34M,
+                OrderDetails = new List<OrderDetail>
+                {
+                    new OrderDetail
+                    {
+                        ProductId = 3,
+                        Quantity = 5,
+                        UnitPrice = 30M
+                    },
+                }
+            });
+        }
+
+        private void AddCustomers(NorthwindSlimContext context)
+        {
+            context.Customers.Add(new Customer
+            {
+                CustomerId = "ALFKI",
+                CompanyName = "Alfreds Futterkiste",
+                ContactName = "Maria Anders",
+                City = "Berlin",
+                Country = "Germany"
+            });
+            context.Customers.Add(new Customer
+            {
+                CustomerId = "ANATR",
+                CompanyName = "Ana Trujillo Emparedados y helados",
+                ContactName = "Ana Trujillo",
+                City = "México D.F.",
+                Country = "Mexico"
+            });
+            context.Customers.Add(new Customer
+            {
+                CustomerId = "ANTON",
+                CompanyName = "Antonio Moreno Taquería",
+                ContactName = "Antonio Moreno",
+                City = "México D.F.",
+                Country = "Mexico"
+            });
+        }
+
+        private void AddCustomerSettings(NorthwindSlimContext context)
+        {
+            context.CustomerSettings.Add(new CustomerSetting
+            {
+                CustomerId = "ALFKI",
+                Setting = "Setting1"
+            });
+            context.CustomerSettings.Add(new CustomerSetting
+            {
+                CustomerId = "ANATR",
+                Setting = "Setting2"
+            });
+            context.CustomerSettings.Add(new CustomerSetting
+            {
+                CustomerId = "ANTON",
+                Setting = "Setting3"
+            });
+        }
+
+        private void AddProducts(NorthwindSlimContext context)
+        {
+            context.Products.Add(new Product
+            {
+                ProductId = 1,
+                CategoryId = 1,
+                ProductName = "Chai",
+                UnitPrice = 23M
+            });
+            context.Products.Add(new Product
+            {
+                ProductId = 2,
+                CategoryId = 1,
+                ProductName = "Chang",
+                UnitPrice = 23M
+            });
+            context.Products.Add(new Product
+            {
+                ProductId = 3,
+                CategoryId = 2,
+                ProductName = "Aniseed Syrup",
+                UnitPrice = 23M
+            });
+            context.Products.Add(new Product
+            {
+                ProductId = 4,
+                CategoryId = 2,
+                ProductName = "Chef Anton's Cajun Seasoning",
+                UnitPrice = 23M
+            });
+        }
+
+        private void AddCategories(NorthwindSlimContext context)
+        {
+            context.Categories.Add(new Category
+            {
+                CategoryId = 1,
+                CategoryName = "Beverages"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 2,
+                CategoryName = "Condiments"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 3,
+                CategoryName = "Confections"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 4,
+                CategoryName = "Dairy Products"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 5,
+                CategoryName = "Grains/Cereals"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 6,
+                CategoryName = "Meat/Poultry"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 7,
+                CategoryName = "Produce"
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = 7,
+                CategoryName = "Seafood"
+            });
+        }
+
+        private void AddEmployees(NorthwindSlimContext context)
+        {
+            var territory1 = new Territory
+            {
+                TerritoryId = "01581",
+                TerritoryDescription = "Westboro"
+            };
+            var territory2 = new Territory
+            {
+                TerritoryId = "01730",
+                TerritoryDescription = "Bedford"
+            };
+            var territory3 = new Territory
+            {
+                TerritoryId = "01833",
+                TerritoryDescription = "Georgetown"
+            };
+            var territory4 = new Territory
+            {
+                TerritoryId = "02116",
+                TerritoryDescription = "Boston"
+            };
+            context.Employees.Add(new Employee
+            {
+                EmployeeId = 1,
+                LastName = "Davolio",
+                FirstName = "Nancy",
+                BirthDate = DateTime.Parse("1948-12-08"),
+                HireDate = DateTime.Parse("1992-05-01"),
+                City = "Seattle",
+                Country = "USA",
+                Territories = new List<Territory> { territory1, territory2, territory3 }
+            });
+            context.Employees.Add(new Employee
+            {
+                EmployeeId = 2,
+                LastName = "Fuller",
+                FirstName = "Andrew",
+                BirthDate = DateTime.Parse("1952-02-19"),
+                HireDate = DateTime.Parse("1992-08-14"),
+                City = "Tacoma",
+                Country = "USA",
+                Territories = new List<Territory> { territory2, territory3 }
+            });
+            context.Employees.Add(new Employee
+            {
+                EmployeeId = 3,
+                LastName = "Leverling",
+                FirstName = "Janet",
+                BirthDate = DateTime.Parse("1963-08-30"),
+                HireDate = DateTime.Parse("1992-05-01"),
+                City = "Kirkland",
+                Country = "USA",
+                Territories = new List<Territory> { territory3, territory4 }
+            });
+            context.Employees.Add(new Employee
+            {
+                EmployeeId = 4,
+                LastName = "Peacock",
+                FirstName = "Margaret",
+                BirthDate = DateTime.Parse("1937-09-19"),
+                HireDate = DateTime.Parse("1993-05-03"),
+                City = "Redmond",
+                Country = "USA",
+                Territories = new List<Territory> { territory1, territory3 }
+            });
+        }
+    }
+}
---
+++ Samples/VS2013/WebApiSample.Relations/WebApiSample.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TrackableEntities;
+using TrackableEntities.EF6;
+using TrackableEntities.Common;
+using WebApiSample.Service.Entities.Contexts;
+using WebApiSample.Service.Entities.Models;
+
+namespace WebApiSample.Service.WebApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private readonly NorthwindSlimContext _dbContext = new NorthwindSlimContext();
+
+        // GET api/Order
+        [ResponseType(typeof(IEnumerable<Order>))]
+        public async Task<IHttpActionResult> GetOrders()
+        {
+	        IEnumerable<Order> orders = await _dbContext.Orders
+				.Include(o => o.Customer)
+                .Include("OrderDetails.Product") // Include details with products
+                .ToListAsync();
+	
+            return Ok(orders);
+        }
+
+        // GET api/Order?customerId=ABCD
+        [ResponseType(typeof(IEnumerable<Order>))]
+        public async Task<IHttpActionResult> GetOrders(string customerId)
+        {
+            IEnumerable<Order> orders = await _dbContext.Orders
+                .Include(o => o.Customer)
+                .Include("OrderDetails.Product") // Include details with products
+                .Where(o => o.CustomerId == customerId)
+                .ToListAsync();
+
+            return Ok(orders);
+        }
+
+        // GET api/Order/5
+        [ResponseType(typeof(Order))]
+        public async Task<IHttpActionResult> GetOrder(int id)
+        {
+	        Order order = await _dbContext.Orders
+				.Include(o => o.Customer)
+                .Include("OrderDetails.Product") // Include details with products
+                .SingleOrDefaultAsync(o => o.OrderId == id);
+	
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // POST api/Order
+        [ResponseType(typeof(Order))]
+        public async Task<IHttpActionResult> PostOrder(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            order.TrackingState = TrackingState.Added;
+            _dbContext.ApplyChanges(order);
+
+            await _dbContext.SaveChangesAsync();
+
+            await _dbContext.LoadRelatedEntitiesAsync(order);
+            order.AcceptChanges();
+            return CreatedAtRoute("DefaultApi", new { id = order.OrderId }, order);
+        }
+
+        // PUT api/Order
+        [ResponseType(typeof(Order))]
+        public async Task<IHttpActionResult> PutOrder(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dbContext.ApplyChanges(order);
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_dbContext.Orders.Any(o => o.OrderId == order.OrderId))
+                {
+                    return Conflict();
+                }
+                throw;
+            }
+
+			await _dbContext.LoadRelatedEntitiesAsync(order);
+			order.AcceptChanges();
+	        return Ok(order);
+        }
+
+        // DELETE api/Order/5
+        public async Task<IHttpActionResult> DeleteOrder(int id)
+        {
+			Order order = await _dbContext.Orders
+                .Include(o => o.OrderDetails) // Include details
+                .SingleOrDefaultAsync(o => o.OrderId == id);
+			if (order == null)
+            {
+                return Ok();
+            }
+
+			order.TrackingState = TrackingState.Deleted;
+			_dbContext.ApplyChanges(order);
+
+            try
+            {
+	            await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_dbContext.Orders.Any(o => o.OrderId == order.OrderId))
+                {
+                    return Conflict();
+                }
+                throw;
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
