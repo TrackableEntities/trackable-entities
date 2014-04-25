2d3f3d4765acd8a6d1e7348efe814a10d845fbb3
tonysneed <tony@tonysneed>
2014-04-25 08:22:35 +0200

    Updated Patterns item templates.

--- Extensions/Common/Wizards/TrackableEntities.ItemWizard/Dialogs.cs
+++ Extensions/Common/Wizards/TrackableEntities.ItemWizard/Dialogs.cs
@@ -7,15 +7,15 @@ namespace TrackableEntities.ItemWizard
         public static class WcfServiceType
         {
             public const string Title = "Trackable WCF Service Type";
-            public const string Message = "Add WCF Service Type with read-write operations using Trackable Entities.";
+            public const string Message = "Add a WCF Service Type with read-write operations using Trackable Entities.";
             public const bool GetDbContextName = true;
             public const int DialogWidth = 650;
         }
 
         public static class EntityController
         {
-            public const string Title = "Entity API Controller";
-            public const string Message = "Add an Entity API Controller to the Trackable WebApi project.";
+            public const string Title = "Entity Web API Controller";
+            public const string Message = "Add an Entity Web API Controller to the Patterns WebApi project.";
             public const bool GetDbContextName = false;
             public const int DialogWidth = 550;
         }
@@ -23,7 +23,7 @@ namespace TrackableEntities.ItemWizard
         public static class EntityRepoClass
         {
             public const string Title = "Entity Repository Class";
-            public const string Message = "Add an Entity Repository Class to the Trackable Service.EF project.";
+            public const string Message = "Add an Entity Repository Class to the Patterns EF project.";
             public const bool GetDbContextName = false;
             public const int DialogWidth = 600;
         }
@@ -31,7 +31,7 @@ namespace TrackableEntities.ItemWizard
         public static class EntityRepoInterface
         {
             public const string Title = "Entity Repository Interface";
-            public const string Message = "Add an Entity Repository Interface to the Service.Persistence project.";
+            public const string Message = "Add an Entity Repository Interface to the Patterns Persistence project.";
             public const bool GetDbContextName = false;
             public const int DialogWidth = 650;
         }
