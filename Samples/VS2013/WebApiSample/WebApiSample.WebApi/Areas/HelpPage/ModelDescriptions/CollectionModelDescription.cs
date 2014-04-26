871184f8cd9cdfd2e4e067368d7badc48c6d177c
tonysneed <tony@tonysneed>
2014-04-26 04:44:32 +0200

    Updated VS2013 WebApi sample Asp.Net NuGet packages.

---
+++ Samples/VS2013/WebApiSample/WebApiSample.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiSample.Service.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
