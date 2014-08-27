65db68af6d442a4cbaf0572f6ecd00893c2bced3
Anthony Sneed <tony@tonysneed.com>
2014-08-27 12:46:58 +0200

    Created nested build scripts. Included TrackableEntities.Client.

--- build.cmd
+++ build.cmd
@@ -16,20 +16,12 @@ if %config% == "" (
 
 set version="%PackageVersion%"
 
-REM Package Restore:
-if "%debug%"=="1" pause
-Source\.nuget\nuget.exe install Source\TrackableEntities.Client\packages.config -OutputDirectory Source\packages -NonInteractive
-Source\.nuget\nuget.exe install Source\TrackableEntities.Client\packages.config -OutputDirectory packages -NonInteractive
-
-REM Build:
-if "%debug%"=="1" pause
-mkdir Build\Source\TrackableEntities.Common\portable-net40+sl5+win8+windowsphone8
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Source\TrackableEntities.Common\TrackableEntities.Common.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=Build\Source\TrackableEntities.Common\portable-net40+sl5+win8+windowsphone8\msbuild.log;Verbosity=Normal /nr:false
+REM TrackableEntities.Common:
+call Build\Scripts\TrackableEntities.Common.cmd
 if not "%errorlevel%"=="0" goto failure
 
-REM Package:
-if "%debug%"=="1" pause
-Source\.nuget\nuget.exe pack "Source\TrackableEntities.Common\TrackableEntities.Common.csproj" -symbols -o Build\Source\TrackableEntities.Common\portable-net40+sl5+win8+windowsphone8 -p Configuration=%config%;PackageVersion=%version%
+REM TrackableEntities.Client:
+call Build\Scripts\TrackableEntities.Client.cmd
 if not "%errorlevel%"=="0" goto failure
 
 :success
