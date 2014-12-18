ea31826d97e607142b38722131560a56c0a5ed9c
Anthony Sneed <tony@tonysneed.com>
2014-12-18 17:36:27 +0100

    Issue #22: Updated NuGet packages in all the sample apps.

--- Samples/VS2013/WebApiSample.Mvvm/WebApiSample.Mvvm/WebApiSample.Mvvm.WebApi/Areas/HelpPage/Controllers/HelpController.cs
+++ Samples/VS2013/WebApiSample.Mvvm/WebApiSample.Mvvm/WebApiSample.Mvvm.WebApi/Areas/HelpPage/Controllers/HelpController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Web.Http;
 using System.Web.Mvc;
+using WebApiSample.Mvvm.WebApi.Areas.HelpPage.ModelDescriptions;
 using WebApiSample.Mvvm.WebApi.Areas.HelpPage.Models;
 
 namespace WebApiSample.Mvvm.WebApi.Areas.HelpPage.Controllers
@@ -10,6 +11,8 @@ namespace WebApiSample.Mvvm.WebApi.Areas.HelpPage.Controllers
     /// </summary>
     public class HelpController : Controller
     {
+        private const string ErrorViewName = "Error";
+
         public HelpController()
             : this(GlobalConfiguration.Configuration)
         {
@@ -39,7 +42,22 @@ namespace WebApiSample.Mvvm.WebApi.Areas.HelpPage.Controllers
                 }
             }
 
-            return View("Error");
+            return View(ErrorViewName);
+        }
+
+        public ActionResult ResourceModel(string modelName)
+        {
+            if (!String.IsNullOrEmpty(modelName))
+            {
+                ModelDescriptionGenerator modelDescriptionGenerator = Configuration.GetModelDescriptionGenerator();
+                ModelDescription modelDescription;
+                if (modelDescriptionGenerator.GeneratedModels.TryGetValue(modelName, out modelDescription))
+                {
+                    return View(modelDescription);
+                }
+            }
+
+            return View(ErrorViewName);
         }
     }
 }
