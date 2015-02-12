1c92d019f58041af0fa504c07832365a6cc985b4
Anthony Sneed <tony@tonysneed.com>
2015-02-12 17:42:24 +0100

    Added nuget push scripts.

---
+++ Build/nuget-proget-symbols.cmd
@@ -0,0 +1,13 @@
+rem VS Setup: http://inedo.com/support/tutorials/debug-into-internal-nuget-packages-with-proget
+
+set source=http://localhost:81/nuget/Default
+set version=2.5.0-alpha5
+set key=Admin:Admin
+
+NuGet.exe push Output\TrackableEntities.Common\TrackableEntities.Common.%version%.symbols.nupkg %key% -Source %source%
+NuGet.exe push Output\TrackableEntities.Client\TrackableEntities.Client.%version%.symbols.nupkg %key% -Source %source%
+NuGet.exe push Output\TrackableEntities.Client.Net4\TrackableEntities.Client.Net4.%version%.symbols.nupkg %key% -Source %source%
+NuGet.exe push Output\TrackableEntities.EF.5\TrackableEntities.EF.5.%version%.symbols.nupkg %key% -Source %source%
+NuGet.exe push Output\TrackableEntities.EF.6\TrackableEntities.EF.6.%version%.symbols.nupkg %key% -Source %source%
+NuGet.exe push Output\TrackableEntities.Patterns\TrackableEntities.Patterns.%version%.symbols.nupkg %key% -Source %source%
+NuGet.exe push Output\TrackableEntities.Patterns.EF.6\TrackableEntities.Patterns.EF.6.%version%.symbols.nupkg %key% -Source %source%
