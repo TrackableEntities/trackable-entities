a2d2b15a7331a5973506cf073cbbd8a1c2b89536
Anthony Sneed <tony@tonysneed.com>
2015-02-16 13:22:55 +0100

    Added JsonObject and DataContract attributes to EntityBase.

--- Source/TrackableEntities.Client.Net45/EntityBaseNet45.cs
+++ Source/TrackableEntities.Client.Net45/EntityBaseNet45.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace TrackableEntities.Client
 {
+    [JsonObject(IsReference = true)]
+    [DataContract]
     public abstract partial class EntityBase
     {
         /// <summary>
         /// Change-tracking state of an entity.
         /// </summary>
         [NotMapped]
+        [DataMember]
         public TrackingState TrackingState { get; set; }
 
         /// <summary>
         /// Properties on an entity that have been modified.
         /// </summary>
         [NotMapped]
+        [DataMember]
         public ICollection<string> ModifiedProperties { get; set; }
 
         /// <summary>
         /// Identifier used for correlation with MergeChanges.
         /// </summary>
         [NotMapped]
+        [DataMember]
         public Guid EntityIdentifier { get; set; }
 
         /// <summary>
