a85084db2e6c56969f93f672ecebda89c868af5b
Anthony Sneed <tony@tonysneed.com>
2015-02-21 17:08:57 +0100

    Created project template for portable shared entities.

--- Extensions/VS2013/Output/Extract/ProjectTemplates/TrackableEntities/TrackableEntities.Shared.Portable/Properties/AssemblyInfo.cs
+++ Extensions/VS2013/Output/Extract/ProjectTemplates/TrackableEntities/TrackableEntities.Shared.Portable/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,17 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("$safeprojectname$")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("60ec04a0-dd73-44d4-ac76-4e2d3c8604e3")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
