01baf44989592acbf2c8be6009ec76050fdcd9c1
stevedelong <a-sdelong@DEV-VS2013.sentrisdev.mantech.com>
2015-07-16 02:57:49 -0400

    Added test cases demonstrating issues #74 and #75
    Added two new test cases (and a trivial multi-parent model) model:
    
    Removed_Added_Items_With_Multiple_Parents_Should_Not_Be_Marked_As_Deleted_Or_Modified
    for issue #74
    
    GetChanges_Should_Not_Leave_Restored_Deleted_Items_With_Multiple_Parents_In_Source_Graph
    for issue #75

---
+++ Source/Tests/TrackableEntities.Client.Tests.Entities/MultiParentModels/ParentType2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackableEntities.Client.Tests.Entities.MultiParentModels
+{
+	public class ParentType2 : EntityBase
+	{
+		public ParentType2()
+		{
+			Children = new ChangeTrackingCollection<MultiParentChild>();
+		}
+
+		public ChangeTrackingCollection<MultiParentChild> Children { get; set; }
+	}
+}
