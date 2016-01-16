ae10be347973f23af32ae7b61e3e9610af49e458
Anthony Sneed <tony@tonysneed.com>
2016-01-16 14:04:26 +0100

    Added PCL project: TrackableEntities.Common.Extended.

---
+++ Source/TrackableEntities.Common.Extended/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TrackableEntities.Common.Extended")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrackableEntities.Common.Extended")]
+[assembly: AssemblyCopyright("Copyright © Tony Sneed 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
