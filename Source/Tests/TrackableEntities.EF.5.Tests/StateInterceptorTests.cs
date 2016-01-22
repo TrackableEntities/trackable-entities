9938cb7a73af2adb648b8fbdd782a5d821006a87
luboshl <github@hlsoft.cz>
2016-01-22 01:27:00 +0100

    Added feature: State interceptors with fluent API. - Allows user to intercept DbContext.ApplyChanges in order to specify the EntityState which should be applied to one or more entities. - Resolves issue #106.

---
+++ Source/Tests/TrackableEntities.EF.5.Tests/StateInterceptorTests.cs
@@ -0,0 +1,105 @@
+﻿#if EF_6
+using System.Data.Entity;
+#else
+using System.Data;
+#endif
+using System.Collections.Generic;
+using System.Linq;
+using TrackableEntities.EF.Tests;
+using TrackableEntities.EF.Tests.Mocks;
+using TrackableEntities.EF.Tests.NorthwindModels;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+#if EF_6
+namespace TrackableEntities.EF6.Tests
+#else
+namespace TrackableEntities.EF5.Tests
+#endif
+{
+    public class StateInterceptorTests
+    {
+        // ReSharper disable once InconsistentNaming
+        private const CreateDbOptions CreateNorthwindDbOptions = CreateDbOptions.DropCreateDatabaseIfModelChanges;
+
+        [Fact]
+        public void Interceptor_Should_Change_Entities_States()
+        {
+            // Arrange
+            var context = TestsHelper.CreateNorthwindDbContext(CreateNorthwindDbOptions);
+            var nw = new MockNorthwind();
+            int[] ids = {1, 2, 3};
+            var products = nw.Products.Where(e => ids.Contains(e.ProductId)).ToList();
+            products.ForEach(e => e.TrackingState = TrackingState.Modified);
+            var cache = new Dictionary<int, EntityState>
+            {
+                { 1, EntityState.Added },
+                { 2, EntityState.Deleted },
+                { 3, EntityState.Unchanged },
+            };
+
+            // Act
+            context
+                .WithInterceptor(new ProductCacheStateInterceptor(cache))
+                .ApplyChanges(products);
+
+            // Assert
+            Assert.Equal(EntityState.Added, context.Entry(products[0]).State);
+            Assert.Equal(EntityState.Deleted, context.Entry(products[1]).State);
+            Assert.Equal(EntityState.Unchanged, context.Entry(products[2]).State);
+        }
+
+        [Fact]
+        public void Interceptor_Should_Not_Change_Entities_States()
+        {
+            // Arrange
+            var context = TestsHelper.CreateNorthwindDbContext(CreateNorthwindDbOptions);
+            var nw = new MockNorthwind();
+            int[] ids = { 4, 5, 6 };
+            var products = nw.Products.Where(e => ids.Contains(e.ProductId)).ToList();
+            products.ForEach(e => e.TrackingState = TrackingState.Modified);
+            var cache = new Dictionary<int, EntityState>
+            {
+                { 1, EntityState.Added },
+                { 2, EntityState.Deleted },
+                { 3, EntityState.Unchanged },
+            };
+
+            // Act
+            context
+                .WithInterceptor(new ProductCacheStateInterceptor(cache))
+                .ApplyChanges(products);
+
+            // Assert
+            Assert.Equal(EntityState.Modified, context.Entry(products[0]).State);
+            Assert.Equal(EntityState.Modified, context.Entry(products[1]).State);
+            Assert.Equal(EntityState.Modified, context.Entry(products[2]).State);
+        }
+
+        #region IStateInterceptor Implementations
+
+        // Look up entity states in a products cache
+        private class ProductCacheStateInterceptor : IStateInterceptor
+        {
+            private readonly Dictionary<int, EntityState> _cache;
+
+            public ProductCacheStateInterceptor(Dictionary<int, EntityState> cache)
+            {
+                _cache = cache;
+            }
+             
+            public EntityState? GetEntityState(ITrackable item, RelationshipType? relationshipType)
+            {
+                var product = item as Product;
+                if (product == null) return null;
+
+                EntityState state;
+                if (_cache.TryGetValue(product.ProductId, out state))
+                    return state;
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/TrackableEntities.EF.5/IStateInterceptor.cs
@@ -0,0 +1,26 @@
+using System.Data.Entity;
+#if EF_6
+#else
+using System.Data;
+#endif
+
+#if EF_6
+namespace TrackableEntities.EF6
+#else
+namespace TrackableEntities.EF5
+#endif
+{
+    /// <summary>
+    /// Interface implemented by interceptors added to <see cref="DbContextExtensions.ApplyChanges(DbContext,ITrackable)"/>.
+    /// </summary>
+    public interface IStateInterceptor
+    {
+        /// <summary>
+        /// Gets state of <paramref name="item"/> based on <paramref name="relationshipType"/>.
+        /// </summary>
+        /// <param name="item">Current item.</param>
+        /// <param name="relationshipType">Relationship of current item.</param>
+        /// <returns>State of <paramref name="item"/> based on <paramref name="relationshipType"/>.</returns>
+        EntityState? GetEntityState(ITrackable item, RelationshipType? relationshipType);
+    }
+}
