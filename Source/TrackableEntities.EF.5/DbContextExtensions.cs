b5f39f2fef34933555ab2da32be05ca50f23cda0
Shimmy <weitzhandler@users.noreply.github.com>
2017-07-11 03:28:05 +0300

    Update DbContextExtensions.cs

--- Source/TrackableEntities.EF.5/DbContextExtensions.cs
+++ Source/TrackableEntities.EF.5/DbContextExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Reflection;
