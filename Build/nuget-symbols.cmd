446b98f890d2a7125623c02fa8057e470e7e3246
Anthony Sneed <tony@tonysneed.com>
2017-11-12 19:01:40 -0600

    Update nuget symbols command. - Update Build badge on ReadMe.

--- Build/nuget-symbols.cmd
+++ Build/nuget-symbols.cmd
@@ -1,11 +1,12 @@
 rem Pushing Symbols packages to NuGet.org
 
-set version=2.5.0-alpha4
-NuGet.exe SetApiKey 5682793e-2994-4016-b7b4-c11be576703b
-NuGet.exe push Output\TrackableEntities.Common\TrackableEntities.Common.%version%.symbols.nupkg
-NuGet.exe push Output\TrackableEntities.Client\TrackableEntities.Client.%version%.symbols.nupkg
-NuGet.exe push Output\TrackableEntities.Client.Net4\TrackableEntities.Client.Net4.%version%.symbols.nupkg
-NuGet.exe push Output\TrackableEntities.EF.5\TrackableEntities.EF.5.%version%.symbols.nupkg
-NuGet.exe push Output\TrackableEntities.EF.6\TrackableEntities.EF.6.%version%.symbols.nupkg
-NuGet.exe push Output\TrackableEntities.Patterns\TrackableEntities.Patterns.%version%.symbols.nupkg
-NuGet.exe push Output\TrackableEntities.Patterns.EF.6\TrackableEntities.Patterns.EF.6.%version%.symbols.nupkg
+set version=2.5.6.1
+set source=https://api.nuget.org/v3/index.json
+nuget SetApiKey 5682793e-2994-4016-b7b4-c11be576703b
+nuget push Output\TrackableEntities.Common\TrackableEntities.Common.%version%.symbols.nupkg -src %source%
+nuget push Output\TrackableEntities.Client\TrackableEntities.Client.%version%.symbols.nupkg -src %source%
+nuget push Output\TrackableEntities.Client.Net4\TrackableEntities.Client.Net4.%version%.symbols.nupkg -src %source%
+nuget push Output\TrackableEntities.EF.5\TrackableEntities.EF.5.%version%.symbols.nupkg -src %source%
+nuget push Output\TrackableEntities.EF.6\TrackableEntities.EF.6.%version%.symbols.nupkg -src %source%
+nuget push Output\TrackableEntities.Patterns\TrackableEntities.Patterns.%version%.symbols.nupkg -src %source%
+nuget push Output\TrackableEntities.Patterns.EF.6\TrackableEntities.Patterns.EF.6.%version%.symbols.nupkg -src %source%
