9d848e7255ab92cd9bfa824f5f3cfba6d52429ac
Anthony Sneed <tony@tonysneed.com>
2017-12-04 09:43:37 -0600

    Add NuGet paclages v2.5.7.

--- Build/nuget-symbols.cmd
+++ Build/nuget-symbols.cmd
@@ -1,7 +1,7 @@
 rem Pushing Symbols packages to NuGet.org
 
-set version=2.5.6.1
-set source=https://api.nuget.org/v3/index.json
+set version=2.5.7
+set source=https://nuget.smbsrc.net/
 nuget SetApiKey 5682793e-2994-4016-b7b4-c11be576703b
 nuget push Output\TrackableEntities.Common\TrackableEntities.Common.%version%.symbols.nupkg -src %source%
 nuget push Output\TrackableEntities.Client\TrackableEntities.Client.%version%.symbols.nupkg -src %source%
--- build-test.cmd
+++ build-test.cmd
@@ -1,4 +1,4 @@
-set PackageVersion=2.5.6.1
+set PackageVersion=2.5.7
 rem set Configuration=Debug
 rem build.cmd -debug
 build.cmd
